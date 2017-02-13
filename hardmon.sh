d1599141d5bff5c7a351ef8597dd35dc73eab130
electronicslover <luizpaulodamaceno@live.com>
2017-02-13 09:02:37 -0200

    Renaming script name.

--- hardmon.sh
+++ hardmon.sh
@@ -3,7 +3,7 @@ while(true)
 do
 hwtemp=$(cat /sys/class/thermal/thermal_zone0/temp)
 hwtemp=$((hwtemp/1000))
-echo CPU Temperature:$hwtempºC
+echo CPU Temperature:$hwtempÂºC
 cpufreq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)
 cpufreq=$((cpufreq/1000))
 echo CPU Frequency: $cpufreq MHz
